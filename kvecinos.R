62282eee9e82c54701e7e0005e873970ad7fff84
Deyban Perez <deyban.perez@gmail.com>
2016-03-03 15:59:47 -0800

    Modelo de kvecinos terminado

--- kvecinos.R
+++ kvecinos.R
@@ -59,9 +59,9 @@ mydata[,"aEconomica"] = NULL
 mydata[,"rating"] = NULL
 mydata[,"sugerencias"] = NULL
 mydata[,"grOdontologicos"] = NULL
+mydata[,"eCivil"] = NULL
 mydata = mydata[-1,] #Deleting outlier
 ################################################
-
 #Calculating probabilities for each element into dataset
 prob_0 = 1/sum(mydata[,"mIngreso"] == 0);
 prob_2 = 1/sum(mydata[,"mIngreso"] == 2);
@@ -70,7 +70,6 @@ prob_3 = 1/sum(mydata[,"mIngreso"] == 3);
 #Allocating space for vector of probabilities
 probabilities = seq(1,nrow(mydata),1)
 ####################################################
-
 aux0 = 0 #Constant for numebr 0
 aux2 = 2 #Constant for number 2
 aux3= 3 #Constant for number 3
@@ -99,23 +98,32 @@ for (i in nrow(mydata))
 #Normalization of features
 mydata_norm = as.data.frame(lapply(mydata[1:ncol(mydata)], normalize))
 #######################################################################
-########################################################
 #Splitting data into training and testing sets
 sub = sample(nrow(mydata), floor(nrow(mydata) * 0.8), prob = probabilities, replace = F)
 training <- mydata[sub, ]
 testing <- mydata[-sub, ]
-trainingLabels = training$mIngreso
-testingLabels = testing$mIngreso
 ######################################################
 #Visializing proportions
 sum(training[,"mIngreso"] == 0)
 sum(training[,"mIngreso"] == 2)
 sum(training[,"mIngreso"] == 3)
 #####################################################
+#Extracting labels for traing and test
+trainingLabels = training$mIngreso
+testingLabels = testing$mIngreso
+####################################################
+#Deleting rows to be predicted
+training[,"mIngreso"] = NULL
+testing[,"mIngreso"] = NULL
+###################################################
+#Normalizing training and testing sets
+training_norm = as.data.frame(lapply(training[1:ncol(training)], normalize))
+testing_norm = as.data.frame(lapply(testing[1:ncol(testing)], normalize))
+##################################################
 #Visualizing Principal Components
-result = PCA(training)
+result = PCA(training_norm)
 #######################################################
-mydata_pred <- knn(train = training, test = testing, cl = trainingLabels, k=13)
+mydata_pred <- knn(train = training_norm, test = testing_norm, cl = trainingLabels, k=14)
 
 #######################################################################
 #Creating confusion matrix
