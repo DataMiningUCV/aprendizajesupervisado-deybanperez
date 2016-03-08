676ede018cfea1bddde70335c0c6e43d6fbaea03
Deyban Perez <deyban.perez@gmail.com>
2016-03-07 17:18:29 -0700

    preprocesamiento

--- hogar.R
+++ hogar.R
@@ -1,9 +1,44 @@
-
+#begin
+#################################################################################
 # Seleccionar google_api.R en su sistema de archivos
 source(file.choose())
+#################################################################################
+# Installing required packages
+install("curl")
+install("xlsx")
+#################################################################################
+#loading required libraries
+library("xlsx")
+#################################################################################
+#Loading dataset
+mydata = read.xlsx("hogares.xlsx", sheetIndex = 1, startRow = 1, endRow = 104, header = T)
+#################################################################################
+#Preprocessing Dataset
+#################################################################################
+#Deleting irrelevant column
+mydata$Foto = NULL
+mydata$Distrito = NULL
+mydata$Piso = NULL
+#################################################################################
+#Removing \n in Column Direction
 
-origen = c("Via Paolo Emilio", "Vancouver BC", "Seattle")
-destino =c("Piazzale Aldo Moro", "San Francisco", "Victoria BC")
+mydata$Dirección = strsplit(as.character(mydata$Dirección), "\n")
+
+for(i in seq(1:nrow(mydata)))
+{
+  mydata$Dirección[i] = paste(mydata$Dirección[[i]], collapse = " ")
+  
+}
+##################################################################################
+#Converting Direccion Column into factor type
+mydata$Dirección = as.character(mydata$Dirección)
+##################################################################################
+#Adding Distamce column to dataset
+mydata$Distance = seq(1:nrow(mydata))
+##################################################################################
+origen = mydata$Dirección[1]
+
+destino =c("Piazzale Aldo Moro")
 
 # Colocar su API Key 
 api_key = "AIzaSyBgTslqWxmNPlZx9-LvmeDrGRnVq8AEXyI"
